3cc8dcaaa22cfa76763fe5336effffbe7bddeb7a
willianctti <willianctti@gmail.com>
2023-11-26 21:27:08 -0300

    finzaliando app

---
+++ LoginScreen/src/routes/index.js
@@ -0,0 +1,33 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Welcome from '../pages/Welcome';
+import SignIn from '../pages/Login';
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+    return (
+        <Stack.Navigator> 
+
+            <Stack.Screen 
+                name='Welcome'
+                component={Welcome}
+                options= {
+                    {
+                        headerShown: false
+                    }
+                }
+            />
+
+               <Stack.Screen 
+                name='SignIn'
+                component={SignIn}
+                options= {
+                    {
+                        headerShown: false
+                    }
+                }
+            />
+
+        </Stack.Navigator>
+    )
+}
